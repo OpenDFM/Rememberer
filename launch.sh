f9fb167b95de76b4d75770113cc006794e920acf
zdy023 <zdy004007@126.com>
2023-03-16 19:21:55 +0800

    ver Mar16thv2
    prepared launch.sh

--- launch.sh
+++ launch.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
 export TOKENIZERS_PARALLELISM=false
-python main.py --task-path ../android_env/apps/wikihow/templates.miniout.microbuffer\
+python main.py --log-dir logs-historyless/\
+			   --task-path ../android_env/apps/wikihow/templates.miniout.microbuffer\
 			   --avd-name Pixel_2_API_30_ga_x64_1\
 			   --tokenizer-path ../deep_rl_zoo/weights/vilt-b32-mlm-tiny-tkn\
-			   --prompt-template prompt_pt.txt
+			   --load-replay history-pools/annotated_pool-auto.yaml\
+			   --item-capacity 500\
+			   --action-capacity 10\
+			   --matcher lcs\
+			   --prompt-template prompts/\
+			   --max-tokens 100\
+			   --manual
