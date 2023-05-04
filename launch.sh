26ecd4fb19893fe067bec3db432d3e4b367d1bc7
zdy023 <zdy004007@126.com>
2023-05-04 20:59:20 +0800

    ver May4thv2
    experiments with different init experiences

--- launch.sh
+++ launch.sh
@@ -8,8 +8,8 @@ python main.py --log-dir logs-historyless/\
 			   --task-path ../android_env/apps/wikihow/templates.miniout.microbuffer.valset.complementary\
 			   --avd-name Pixel_2_API_30_ga_x64_1\
 			   --tokenizer-path weights/vilt-b32-mlm-tiny-tkn\
-			   --load-replay history-pools/init_pool.qu.2023-05-04T11:11:53.2.yaml\
-			   --save-replay history-pools/init_pool.qu."$date_str".%d.yaml\
+			   --load-replay history-pools/init_pool3.qu.2023-05-04T19:23:50.2.yaml\
+			   --save-replay history-pools/init_pool3.qu."$date_str".%d.yaml\
 			   --item-capacity 500\
 			   --action-capacity 10\
 			   --matcher lcs+inspat\
