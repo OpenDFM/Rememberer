424982f3cecdd66ad5b65e891f1cbcc2827ebb52
zdy023 <zdy004007@126.com>
2023-05-06 11:00:04 +0800

    ver May6th
    all the experiments of W9 is ok

--- launch.sh
+++ launch.sh
@@ -5,10 +5,10 @@ date_str=$(date +%Y-%m-%dT%H:%M:%S)
 export TOKENIZERS_PARALLELISM=false
 python main.py --log-dir logs/\
 			   --train-path ../../../android_env/apps/wikihow/templates.miniout.microbuffer.complementary.trainset\
-			   --task-path ../../../android_env/apps/wikihow/templates.miniout.microbuffer.valset\
+			   --task-path ../../../android_env/apps/wikihow/templates.miniout.microbuffer.valset.complementary\
 			   --avd-name Pixel_2_API_30_ga_x64_1\
 			   --tokenizer-path weights/vilt-b32-mlm-tiny-tkn\
-			   --load-replay history-pools/init_pool.q.yaml\
+			   --load-replay history-pools/init_pool.qu.2023-05-05T23:49:24.2.yaml\
 			   --save-replay history-pools/init_pool.qu."$date_str".%d.yaml\
 			   --item-capacity 500\
 			   --action-capacity 10\
@@ -18,6 +18,5 @@ python main.py --log-dir logs/\
 			   --max-tokens 100\
 			   --stop "Discouraged"\
 			   --request-timeout 10.\
-			   --train\
 			   --starts-from 0\
 			   --epochs 3
