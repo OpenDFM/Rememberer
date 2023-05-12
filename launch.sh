9b979a140f5e69d4a0cf038506f06295aa6e512d
zdy023 <zdy004007@126.com>
2023-05-12 15:43:35 +0800

    ver May12th
    reset the setting

--- launch.sh
+++ launch.sh
@@ -8,15 +8,16 @@ python main.py --log-dir logs/\
 			   --task-path ../../../android_env/apps/wikihow/templates.miniout.microbuffer.valset.complementary\
 			   --avd-name Pixel_2_API_30_ga_x64_1\
 			   --tokenizer-path weights/vilt-b32-mlm-tiny-tkn\
-			   --load-replay history-pools/init_pool.qu.2023-05-10T09:37:53.2.yaml\
+			   --load-replay history-pools/init_pool.q.yaml\
 			   --save-replay history-pools/init_pool.qu."$date_str".%d.yaml\
 			   --item-capacity 500\
 			   --action-capacity 10\
-			   --matcher inspat\
+			   --matcher lcs+inspat\
 			   --n-step-flatten 1\
 			   --prompt-template prompts/\
 			   --max-tokens 100\
 			   --stop "Discouraged"\
 			   --request-timeout 10.\
+			   --train\
 			   --starts-from 0\
 			   --epochs 3
